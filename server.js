c7c1e31c4fb4a2d0780d677f4f8b7341845100ab
Ruan Mello <ruan.mello0123@gmail.com>
2022-09-15 21:23:16 -0300

    Alteração do port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
